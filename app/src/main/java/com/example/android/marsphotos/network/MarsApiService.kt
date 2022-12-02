ae3e9c58991b6fc11f2ae563dfc927becc22b425
lenovo <suryahadikusuma351@gmail.com>
2022-12-02 14:08:01 +0700

    comitee

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -15,15 +15,12 @@
  */
 
 package com.example.android.marsphotos.network
-
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
-
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
-
 /**
  * Buat objek Moshi yang akan digunakan Retrofit, pastikan untuk menambahkan adaptor Kotlin
  * Kompatibilitas penuh Kotlin.
@@ -31,7 +28,6 @@ private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
-
 /**
  * Gunakan pembuat Retrofit untuk membuat objek retrofit menggunakan konverter Moshi dengan Moshi kami
  * objek.
@@ -40,7 +36,6 @@ private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
-
 /**
  * Antarmuka publik yang menampilkan metode [getPhotos].
  */
